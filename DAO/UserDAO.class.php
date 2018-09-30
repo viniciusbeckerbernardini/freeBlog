01e8d6449b43bc8cf59900db18c8edb4caeba75d
bk3301 <leonardo.oliveira98@gmail.com>
2018-09-30 20:06:25 -0300

    Adicionandas funções CRUD ao userDAO

---
+++ DAO/UserDAO.class.php
@@ -0,0 +1,40 @@
+<?php
+/**
+*@classname UsuaryDAO
+*@author Leonardo Pereira Oliveira
+*/
+// Creating Data Acess Object of User
+class UserDAO extends User{
+// Creating Register function
+	public function registerUser(){
+		$sql = new SQL("freeBlog","localhost","root","");
+		$resultado = $sql->query("INSERT INTO tb_user (name,email,password,userType)
+		VALUES(:NAME,:EMAIL,:PASSWORD,:USERTYPE)",
+		array(":NOME"=>$this->getName(),
+			  ":EMAIL"=>$this->getEmail(),
+				":PASSWORD"=>$this->getPassword(),
+			  ":USERTYPE"=>$this->getUserType()));
+	}
+// Creating List function
+	public function listUser(){
+		$sql = new SQL("freeBlog","localhost","root","");
+		$resultado = $sql->select("SELECT * FROM tb_user");
+	}
+// Creating Update function
+	public function updateUser(){
+		$sql = new SQL("freeBlog","localhost","root","");
+		$resultado = $sql->query("UPDATE tb_user SET name = :NAME, email = :EMAIL, password = :PASSWORD, userType = :USERTYPE where userId = :ID",
+			array(
+			  ":ID" => $this->getUserId(),
+			  ":NOME"=>$this->getName(),
+			  ":EMAIL"=>$this->getEmail(),
+			  ":PASSWORD"=>$this->getPassword(),
+				":USERTYPE"=>$this->getUserType()));
+	}
+// Creating Delete function
+	public  function deleteUser($id){
+		$sql = new SQL("freeBlog","localhost","root","");
+		$comando = $sql->query("DELETE FROM tb_user where userId = :ID",
+		array(":ID"=>$this->getUsuarioID($id)));
+	}
+}
