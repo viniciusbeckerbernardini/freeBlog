ab5898a832bbbf7700593ef5048c82615ad1d400
bk3301 <leonardo.oliveira98@gmail.com>
2018-10-05 14:49:18 -0300

    Corrigindo UserDAO

--- DAO/UserDAO.class.php
+++ DAO/UserDAO.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
-*@classname UsuaryDAO
+*@classname UserDAO
 *@author Leonardo Pereira Oliveira
 */
 // Creating Data Acess Object of User
@@ -8,7 +8,7 @@ class UserDAO extends User{
 // Creating Register function
 	public function registerUser(){
 		$sql = new SQL("localhost","freeBlog","root","");
-		$resultado = $sql->query("INSERT INTO tb_user (name,email,password,userType)
+		$statement = $sql->query("INSERT INTO tb_user (name,email,password,userType)
 		VALUES(:NAME,:EMAIL,:PASSWORD,:USERTYPE)",
 		array(":NOME"=>$this->getName(),
 			  ":EMAIL"=>$this->getEmail(),
@@ -18,12 +18,12 @@ class UserDAO extends User{
 // Creating List function
 	public function listUser(){
 		$sql = new SQL("localhost","freeBlog","root","");
-		$resultado = $sql->select("SELECT * FROM tb_user");
+		$statement = $sql->select("SELECT * FROM tb_user");
 	}
 // Creating Update function
 	public function updateUser(){
 		$sql = new SQL("localhost","freeBlog","root","");
-		$resultado = $sql->query("UPDATE tb_user SET name = :NAME, email = :EMAIL, password = :PASSWORD, userType = :USERTYPE where userId = :ID",
+		$statement = $sql->query("UPDATE tb_user SET name = :NAME, email = :EMAIL, password = :PASSWORD, userType = :USERTYPE where userId = :ID",
 			array(
 			  ":ID" => $this->getUserId(),
 			  ":NOME"=>$this->getName(),
@@ -34,7 +34,7 @@ class UserDAO extends User{
 // Creating Delete function
 	public  function deleteUser($id){
 		$sql = new SQL("localhost","freeBlog","root","");
-		$comando = $sql->query("DELETE FROM tb_user where userId = :ID",
+		$statement = $sql->query("DELETE FROM tb_user where userId = :ID",
 		array(":ID"=>$this->getUsuarioID($id)));
 	}
 }
