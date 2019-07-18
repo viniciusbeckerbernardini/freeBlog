fc9cf9a2de047c8c4d33f6ade9ffb68780c99961
viniciusbeckerbernardini <vbkmma@gmail.com>
2019-07-18 00:34:01 -0300

    Home screen started development

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php 
 require_once('inc'.DIRECTORY_SEPARATOR.'config.php');
-require_once('view'.DIRECTORY_SEPARATOR.'templates'.DIRECTORY_SEPARATOR.'header.php');
 router();
-require_once('view'.DIRECTORY_SEPARATOR.'templates'.DIRECTORY_SEPARATOR.'footer.php');
-
 ?>
--- view/index.php
+++ view/index.php
@@ -1,11 +1,45 @@
 <?php
 // Requesting the config file
-require_once('inc'.DIRECTORY_SEPARATOR.'config.php');
+require_once('inc' . DIRECTORY_SEPARATOR . 'config.php');
 // Requesting the header file
-require_once('templates'.DIRECTORY_SEPARATOR.'header.php');
-// Searching the posts
+require_once('templates' . DIRECTORY_SEPARATOR . 'header.php');
 ?>
-<h1 class="center">HOME</h1>
-<?php 
+
+<div class="empty-space"></div>
+<div class="row">
+    <div class="col s12">
+        <div class="col s6">
+            <div class="fb-cb" style="background:url('/view/library/img/photo-1.jpg') no-repeat center center;">
+                <div class="fb-cb-hv">
+                    <p>Programador ama café? <br> Descubra porque amamos tanto algo tão ruim</p>
+                </div>
+            </div>
+        </div>
+        <div class="col s6">
+            <div class="col s12">
+                <div class="fb-cb" style="background:url('/view/library/img/photo-2.jpg') no-repeat center center;">
+                    <div class="fb-cb-hv">
+                        <p>
+                        Você é programador?
+                        <br>
+                        Vamos descobrir agora
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12">
+                <div class="fb-cb" style="background:url('/view/library/img/photo-3.jpg') no-repeat center center;">
+                    <div class="fb-cb-hv">
+                        <p>Tiroteio e renteri <br> Galo véio ruim de briga </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php
 // Requesting the footer file
-require_once('templates'.DIRECTORY_SEPARATOR.'footer.php');
+require_once('templates' . DIRECTORY_SEPARATOR . 'footer.php');
